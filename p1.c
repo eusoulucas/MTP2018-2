4c3b0116565a613cfcd0cd52cbf07a8ae0198943
eusoulucas <42423212+eusoulucas@users.noreply.github.com>
2018-09-11 22:22:36 -0300

    Add files via upload

---
+++ p1.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+int main()
+{
+    int estado = 0,i = 0;
+    char bits[236];
+
+        scanf("%s", bits);
+
+    while(bits[i] != '\0')
+    {
+        if(estado == 0 && bits[i] == 0)
+        {
+            estado = 0;
+        }
+        else if(estado == 0 && bits[i] == 1)
+        {
+            estado = 1;
+        }
+        else if(estado == 1 && bits[i] == 0)
+        {
+            estado = 2;
+        }
+        else if(estado == 1 && bits[i] == 1)
+        {
+            estado = 0;
+        }
+        else if(estado == 2 && bits[i] == 0)
+        {
+            estado = 1;
+        }
+        else if(estado == 2 && bits[i] == 1)
+        {
+            estado = 2;
+        }
+        i++;
+    }
+    if(estado == 0)
+    {
+        printf("\nEh multiplo de 3. ");
+    }
+    else
+    {
+        printf("\nNao eh multiplo de 3.") ;
+    }
+
+    return 0;
+}
